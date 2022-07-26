84c62aff68577a7d86818c5774926bd0d8af90b9
Cássio Pereira <cassiorodp@gmail.com>
2022-07-26 15:21:41 -0300

    feat: add hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there, <br/>
+        I'm Cássio Pereira
+      </SectionTitle>
+      <SectionText>
+        I'm a web development student. Interested in front-end development. Passionate about tools that allow you to create amazing things. <br/> Main skills: React.js, HTML, CSS, JavaScript.
+      </SectionText>
+      <Button onClick={() => window.location.href = "mailto:cassiorodp@gmail.com"}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
