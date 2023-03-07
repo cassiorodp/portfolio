6fb256bd8489a06c3b02f2e253b16d714238c856
Cássio Pereira <cassiorodp@gmail.com>
2023-03-07 17:58:59 -0300

    feat: add blog App project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,18 @@
 export const projects = [
   {
     title: 'Pokedex App',
-    description: "Using React, CSS, HTML & JS, with the resources of the pokeAPI. We've created a working pokedex so you can search that pokemon of your childhood! All of this with the original colors of the franchise.",
-      image: '/images/1.png',
-      tags: ['React', 'HTML', 'CSS'],
+    description:
+      "Using React, CSS, HTML & JS, with the resources of the pokeAPI. We've created a working pokedex so you can search that pokemon of your childhood! All of this with the original colors of the franchise.",
+    image: '/images/1.png',
+    tags: ['React', 'HTML', 'CSS'],
     source: 'https://cassiorodp.github.io/pokemon-world/',
     visit: 'https://github.com/cassiorodp/pokemon-world',
     id: 0,
   },
   {
     title: 'Buy-Please',
-    description:"An App built with React to emulate a working e-commerce, this demo was made to pratice mainly react routes & react props. ",
+    description:
+      'An App built with React to emulate a working e-commerce, this demo was made to pratice mainly react routes & react props. ',
     image: '/images/2.png',
     tags: ['React', 'React Router', 'CSS'],
     source: 'https://cassiorodp.github.io/buy-please/',
@@ -19,16 +21,18 @@ export const projects = [
   },
   {
     title: 'Star Wars Planet Search',
-    description: "This App was made to pratice some React Hooks and Context API, as well to implement a cool star wars planets information table. ",
-      image: '/images/3.png',
-      tags: ['React', 'React Hooks', 'Context API'],
+    description:
+      'This App was made to pratice some React Hooks and Context API, as well to implement a cool star wars planets information table. ',
+    image: '/images/3.png',
+    tags: ['React', 'React Hooks', 'Context API'],
     source: 'https://cassiorodp.github.io/star-wars-planet-search/',
     visit: 'https://github.com/cassiorodp/star-wars-planet-search',
     id: 2,
   },
   {
     title: "Let's Cook",
-    description: "A mobile App of recipes powered by thecocktaildb and www.themealdb.com API's. Made mainly to pratice redux and mobile-first development. ",
+    description:
+      "A mobile App of recipes powered by thecocktaildb and www.themealdb.com API's. Made mainly to pratice redux and mobile-first development. ",
     image: '/images/4.png',
     tags: ['React', 'React Hooks', 'Redux'],
     source: 'https://cassiorodp.github.io/recipe-app/#/',
@@ -36,21 +40,32 @@ export const projects = [
     id: 3,
   },
   {
-    title: "José Delivery",
-    description: "José Delivery App is an educational app for you to emulate a real deliver store! Here you can act like a customer or a seller!",
+    title: 'José Delivery',
+    description:
+      'José Delivery App is an educational app for you to emulate a real deliver store! Here you can act like a customer or a seller!',
     image: '/images/5.png',
     tags: ['React', 'Redux', 'Node', 'MySQL', 'Sequelize'],
     source: 'https://jose-delivery-app.vercel.app/login',
     visit: 'https://github.com/cassiorodp/delivery-app',
     id: 4,
   },
+  {
+    title: 'Blog App',
+    description:
+      'Nextjs Blog app is an app that emulate a functional blog using all next js features!',
+    image: '/images/6.png',
+    tags: ['Next.js', 'MongoDB', 'HTML5', 'CSS3'],
+    source: 'https://nextjs-blog-app-livid.vercel.app/',
+    visit: 'https://github.com/cassiorodp/nextjs_blog_app',
+    id: 5,
+  },
 ];
 
 export const TimeLineData = [
-  { year: 1997, text: 'Hello World', },
-  { year: 2015, text: 'Got into university', },
-  { year: 2019, text: 'Graduated in civil engineering', },
+  { year: 1997, text: 'Hello World' },
+  { year: 2015, text: 'Got into university' },
+  { year: 2019, text: 'Graduated in civil engineering' },
   { year: 2020, text: 'Worked as an environmental analyst' },
-  { year: 2021, text: 'Started studying web develoment', },
-  { year: 2022, text: 'Started to work as a web developer at D3 Company', }
+  { year: 2021, text: 'Started studying web develoment' },
+  { year: 2022, text: 'Started to work as a web developer at D3 Company' },
 ];
